6f1a0109fc07c89dff78c2f4e69f78551edb6091
bakenGao <bakengao@gmail.com>
2014-09-17 16:48:38 +0800

    Initial Commit

---
+++ TestForTW/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestForTW
+//
+//  Created by Mac on 14-9-17.
+//  Copyright (c) 2014年 MyApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MAAppDelegate class]));
+    }
+}
